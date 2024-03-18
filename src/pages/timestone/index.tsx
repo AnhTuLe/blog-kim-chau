6b77ff6a12e8daf8635fe3258b5284094ddbf531
anhlt <letuanh821993@gmail.com>
2024-03-18 23:22:17 +0700

    defined some folder

---
+++ src/pages/timestone/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface TimeStonePageProps {}
+
+const TimeStonePage: FC<TimeStonePageProps> = (props) => {
+  return <div>TimeStone Page</div>;
+};
+
+export default TimeStonePage;
---
+++ src/pages/travel/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface TravelPageProps {}
+
+const TravelPage: FC<TravelPageProps> = (props) => {
+  return <div>Travel Page</div>;
+};
+
+export default TravelPage;
